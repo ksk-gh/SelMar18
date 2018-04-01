235e37dc1a54a939156cf9e6fea6a5e0b390bbfa
TestLeafPages <homework@testleaf.com>
2018-04-01 18:06:07 +0530

    Add files via upload

---
+++ tchallenge/DownloadFiles.java
@@ -0,0 +1,37 @@
+package tchallenge;
+
+import java.util.HashMap;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+@Test
+public class DownloadFiles {
+	RemoteWebDriver driver;
+	DesiredCapabilities cap;
+	@SuppressWarnings("deprecation")
+	public void downloadFiles()	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+
+		String downloadFilepath = "K:\\";
+		HashMap<String, Object> setPath = new HashMap<String, Object>();	
+		setPath.put("download.default_directory", downloadFilepath); //To set path
+		setPath.put("safebrowsing.enabled", "false"); // To disable security check
+
+		HashMap<String, Object> chromeOptionsMap = new HashMap<String, Object>();
+		options.setExperimentalOption("prefs", setPath);
+		options.addArguments("--disable-extensions"); //to disable browser extension popup
+		
+		cap = DesiredCapabilities.chrome(); //Adding capabilities
+		cap.setCapability(ChromeOptions.CAPABILITY, chromeOptionsMap);
+		cap.setCapability(ChromeOptions.CAPABILITY, options);
+
+		driver = new ChromeDriver(cap);
+		driver.manage().window().maximize();
+		driver.get("https://www.win-rar.com/predownload.html");
+		driver.findElementByLinkText("Download WinRAR").click();
+	}
+}
